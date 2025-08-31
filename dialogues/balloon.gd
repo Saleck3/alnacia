4e5e652a58913f1e8260667ca6d470f1f85b9284
sofiamgl <sofiamgl@gmail.com>
2025-09-01 01:05:27 +0200

    Agregado prototipo de retrato personajes

--- dialogues/balloon.gd
+++ dialogues/balloon.gd
@@ -42,6 +42,9 @@ var mutation_cooldown: Timer = Timer.new()
 ## The base balloon anchor
 @onready var balloon: Control = %Balloon
 
+## The label showing the portrait of the currently speaking character
+@onready var portrait: TextureRect = $Balloon/MarginContainer/PanelContainer/MarginContainer/HBoxContainer/CharPortrait
+
 ## The label showing the name of the currently speaking character
 @onready var character_label: RichTextLabel = %CharacterLabel
 
@@ -97,7 +100,16 @@ func apply_dialogue_line() -> void:
 
 	character_label.visible = not dialogue_line.character.is_empty()
 	character_label.text = tr(dialogue_line.character, "dialogue")
-
+	
+	character_label.visible = not dialogue_line.character.is_empty()
+	character_label. text = tr(dialogue_line.character, "dialogue")
+	
+	var portrait_path: String = "res://assets/characters/%s.png" % dialogue_line.character
+	if ResourceLoader.exists(portrait_path):
+		portrait.texture = load(portrait_path)
+	else: 
+		portrait.texture = null
+		
 	dialogue_label.hide()
 	dialogue_label.dialogue_line = dialogue_line
 
